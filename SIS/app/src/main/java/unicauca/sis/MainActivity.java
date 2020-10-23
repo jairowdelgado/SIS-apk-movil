af0092d80a370be18445b8e6ff089a9ff4e74e75
jairowdelgado <49734057+jairowdelgado@users.noreply.github.com>
2020-10-23 11:24:18 -0500

    registrar e ingresar

--- SIS/app/src/main/java/unicauca/sis/MainActivity.java
+++ SIS/app/src/main/java/unicauca/sis/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void ingresar(View view){
-        Intent menuIn= new Intent(this,MenuInicioAdmin.class);
+        Intent menuIn= new Intent(this,MenuIngresar.class);
         startActivity(menuIn);
 
     }
